b8a27ef70f28929b88510c4a5fe1c060e002f5b6
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-07 16:59:31 -0400

    spring-init

---
+++ Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
