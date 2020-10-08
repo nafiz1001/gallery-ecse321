ced9c2b248e00b5b534d0345ee4a8e2f1848b7ba
HALUK CALIN <haluk.calin@mail.mcgill.ca>
2020-10-08 23:08:54 +0300

    Revenu Class Persistence Testsv1

---
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/RevenuRepository.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.gallery.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.gallery.model.Revenu;
+
+public interface RevenuRepository extends CrudRepository<Revenu, String> {
+
+	Revenu findRevenuById(String id);
+}
---
+++ Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/dao/RevenuPersistenceTests.java
@@ -0,0 +1,80 @@
+package ca.mcgill.ecse321.gallery.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.gallery.model.Revenu;
+import ca.mcgill.ecse321.gallery.model.Account;
+import ca.mcgill.ecse321.gallery.model.Listing;
+
+
+
+public class RevenuPersistenceTests {
+	
+	@Autowired
+	private RevenuRepository revenuRepository;
+	
+	@BeforeEach
+	public void clearDatabse() {
+		revenuRepository.deleteAll();
+	}
+	
+	@AfterEach
+	public void clearDatabase() {
+		revenuRepository.deleteAll();
+	}
+	
+	@Test
+	public void testCreateAndLoadRevenu() {
+		Account account1 = new Account();
+		Revenu revenu1 = new Revenu();
+		Listing listing1 = new Listing();
+		revenu1.setComission(1);
+		revenu1.setListingPrice(2);
+		revenu1.setAccount(account1);
+		revenu1.setListing(listing1);
+		revenu1.setId("d");
+		
+		revenuRepository.save(revenu1);
+		
+		Revenu revenu2 = revenuRepository.findRevenuById("b");
+		
+		assertNotNull(revenu2);
+		assertEquals(revenu1.getComission(), revenu1.getComission());
+	}
+	
+	@Test
+	public void testUpdateRevenu() {
+		Account account1 = new Account(); 
+		Revenu revenu1 = new Revenu();
+		Listing listing1 = new Listing();
+		revenu1.setComission(1);
+		revenu1.setListingPrice(2);
+		revenu1.setAccount(account1);
+		revenu1.setListing(listing1);
+		revenu1.setId("d");
+		
+		revenu1 = revenuRepository.save(revenu1);
+		
+		revenu1.setComission(2);
+		
+		revenu1 = revenuRepository.save(revenu1);
+		
+		assertEquals(revenu1.getComission(),1);
+	}
+}
