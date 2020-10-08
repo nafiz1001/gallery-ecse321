6a90a1b0c17670efa81cbdc5c34c51f387b9ee82
Thomas <thomasrocks128@me.com>
2020-10-08 17:41:42 -0400

    Fixed ArtRepository

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/ArtRepository.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/ArtRepository.java
@@ -3,10 +3,9 @@ package ca.mcgill.ecse321.gallery.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.gallery.model.Art;
-import ca.mcgill.ecse321.gallery.model.Gallery;
 
 public interface ArtRepository extends CrudRepository<Art, String> {
-Art findArtByID(String id);
+Art findArtById(String id);
 	
 	void deleteAll();
 }
