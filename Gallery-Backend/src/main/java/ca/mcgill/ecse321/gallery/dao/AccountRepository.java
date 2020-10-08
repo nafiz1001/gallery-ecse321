afc620f6b6f5babc3b2d58c660a8e28f433afa08
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-08 17:20:29 -0400

    create AccountRepository.java

---
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/AccountRepository.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse321.gallery.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.gallery.model.Account;
+
+public interface AccountRepository extends CrudRepository<Account, String>{
+	Account findAccountByUsername(String id);
+	
+	void deleteAll();
+}
