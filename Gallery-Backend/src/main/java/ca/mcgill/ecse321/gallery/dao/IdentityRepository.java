0050d5b0688d7a225cb7ef671037d274c91f77e6
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-08 13:15:01 -0400

    use CRUD instead

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/IdentityRepository.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/IdentityRepository.java
@@ -5,47 +5,14 @@ import java.sql.Time;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.gallery.model.Account;
 import ca.mcgill.ecse321.gallery.model.Identity;
 import ca.mcgill.ecse321.gallery.model.Payment;
 
-@Repository
-public class IdentityRepository {
-
-	@Autowired
-	EntityManager entityManager;
+public interface IdentityRepository extends CrudRepository<Identity, String>{
+	Identity findIdentityByEmail(String email);
 	
-	@Transactional
-	public Identity createIdentity(String email, Account account) {
-		Identity identity = new Identity(email, account);
-		entityManager.persist(identity);
-		return identity;
-	}
-	
-	@Transactional
-	public Identity getIdentity(String email) {
-		Identity identity = entityManager.find(Identity.class, email);
-		return identity;
-	}
-	
-	@Transactional
-	public Identity removeIndentity(String email) {
-		Identity identity = entityManager.find(Identity.class, email);
-		entityManager.remove(identity);
-		return identity;
-	}
-	
-	@Transactional
-	public void deleteAll() {
-		Query deleteQuery = entityManager.createQuery("DELETE FROM Identity;");
-		deleteQuery.executeUpdate();
-	}
+	void deleteAll();
 }
--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/PaymentRepository.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/PaymentRepository.java
@@ -5,13 +5,8 @@ import java.sql.Time;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.gallery.model.Address;
 import ca.mcgill.ecse321.gallery.model.DeliveryType;
@@ -20,37 +15,9 @@ import ca.mcgill.ecse321.gallery.model.Listing;
 import ca.mcgill.ecse321.gallery.model.Payment;
 import ca.mcgill.ecse321.gallery.model.PaymentType;
 
-@Repository
-public class PaymentRepository {
 
-	@Autowired
-	EntityManager entityManager;
-
-	@Transactional
-	public Payment createPayment(String transactionNumber, Date paymentDate, PaymentType paymentType,
-			DeliveryType deliveryType, Address address, Set<Listing> listing, Identity identity) {
-		
-		Payment p = new Payment(transactionNumber, paymentDate, paymentType, deliveryType, address, listing, identity);
-		entityManager.persist(p);
-		return p;
-	}
-
-	@Transactional
-	public Payment getPayment(long confirmationNumber) {
-		Payment p = entityManager.find(Payment.class, confirmationNumber);
-		return p;
-	}
+public interface PaymentRepository extends CrudRepository<Payment, Long>{
+	Payment findPaymentByConfirmationNumber(long confirmationNumber);
 	
-	@Transactional
-	public Payment removePayment(String confirmationNumber) {
-		Payment p = entityManager.find(Payment.class, confirmationNumber);
-		entityManager.remove(p);
-		return p;
-	}
-	
-	@Transactional
-	public void deleteAll() {
-		Query deleteQuery = entityManager.createQuery("DELETE FROM Payment");
-		deleteQuery.executeUpdate();
-	}
+	void deleteAll();
 }
