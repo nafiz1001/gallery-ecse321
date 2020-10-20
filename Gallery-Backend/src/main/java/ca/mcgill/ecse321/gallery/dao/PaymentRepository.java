730e93a13382592da68aa228a4c3553fd7562c36
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-20 18:01:51 -0400

    add rest annotation to PaymentRepository

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/PaymentRepository.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/PaymentRepository.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import ca.mcgill.ecse321.gallery.model.Address;
 import ca.mcgill.ecse321.gallery.model.DeliveryType;
@@ -16,6 +17,7 @@ import ca.mcgill.ecse321.gallery.model.Payment;
 import ca.mcgill.ecse321.gallery.model.PaymentType;
 
 
+@RepositoryRestResource(collectionResourceRel = "payment_data", path = "payment_data")
 public interface PaymentRepository extends CrudRepository<Payment, Long>{
 	Payment findPaymentByConfirmationNumber(long confirmationNumber);
 	
