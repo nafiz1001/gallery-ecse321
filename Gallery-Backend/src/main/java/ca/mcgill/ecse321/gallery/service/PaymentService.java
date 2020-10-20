a68744d3c3f57ecc0c0742291ec285ef56d6e0f0
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-20 18:02:38 -0400

    create PaymentService.java

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/PaymentService.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/PaymentService.java
@@ -1,31 +1,38 @@
 package ca.mcgill.ecse321.gallery.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.gallery.dao.PaymentRepository;
+import ca.mcgill.ecse321.gallery.model.DeliveryType;
 import ca.mcgill.ecse321.gallery.model.Payment;
+import ca.mcgill.ecse321.gallery.model.PaymentType;
 
+@Service
 public class PaymentService {
 	@Autowired
 	PaymentRepository paymentRepository;
 	
 	@Transactional
-	public Payment createPayment(String transactionNumber) {
-		Payment Payment = new Payment();
-		Payment.setTransactionNumber(transactionNumber);
-		paymentRepository.save(Payment);
-		return Payment;
+	public Payment pay(String transactionNumber) {
+		Payment payment = new Payment();
+		payment.setTransactionNumber(transactionNumber);
+		payment.setDeliveryType(DeliveryType.PICKUP);
+		payment.setPaymentType(PaymentType.CREDIT_CARD);
+		payment = paymentRepository.save(payment);
+		return payment;
 	}
 
 	@Transactional
 	public Payment getPayment(long confirmationNumber) {
-		Payment Payment = paymentRepository.findPaymentByConfirmationNumber(confirmationNumber);
-		return Payment;
+		Payment payment = paymentRepository.findPaymentByConfirmationNumber(confirmationNumber);
+		return payment;
 	}
 
 	@Transactional
