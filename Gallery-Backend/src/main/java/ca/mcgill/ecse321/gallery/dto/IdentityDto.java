64915ee45038721f2572cc2d089ac7078afa5245
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-21 08:46:05 -0400

    define IdentityDto

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dto/IdentityDto.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dto/IdentityDto.java
@@ -1,5 +1,23 @@
 package ca.mcgill.ecse321.gallery.dto;
 
 public class IdentityDto {
+	private String email;
 
+	public void setEmail(String value) {
+		this.email = value;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	private AccountDto account;
+
+	public AccountDto getAccount() {
+		return this.account;
+	}
+
+	public void setAccount(AccountDto account) {
+		this.account = account;
+	}
 }
