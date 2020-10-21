0427f138c8fd1d04a4eb4c863789e171599d37c3
nafiz1001 <nafiz.islam1001@gmail.com>
2020-10-21 08:42:58 -0400

    define AddressDto

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dto/AddressDto.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dto/AddressDto.java
@@ -1,5 +1,64 @@
 package ca.mcgill.ecse321.gallery.dto;
 
 public class AddressDto {
+	private String streetNumber;
+
+	public void setStreetNumber(String value) {
+		this.streetNumber = value;
+	}
+
+	public String getStreetNumber() {
+		return this.streetNumber;
+	}
+
+	private String street;
+
+	public void setStreet(String value) {
+		this.street = value;
+	}
+
+	public String getStreet() {
+		return this.street;
+	}
+
+	private String city;
+
+	public void setCity(String value) {
+		this.city = value;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	private String province;
+
+	public void setProvince(String value) {
+		this.province = value;
+	}
+
+	public String getProvince() {
+		return this.province;
+	}
+
+	private String postalCode;
+
+	public void setPostalCode(String value) {
+		this.postalCode = value;
+	}
+
+	public String getPostalCode() {
+		return this.postalCode;
+	}
+
+	private String id;
+
+	public void setId(String value) {
+		this.id = value;
+	}
+
+	public String getId() {
+		return this.id;
+	}
 
 }
