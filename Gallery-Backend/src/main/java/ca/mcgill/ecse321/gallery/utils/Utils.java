3d3f08a57f20c21dfb82424dca44c398f1d29047
Antonia Nistor <antonianistor@hotmail.fr>
2020-10-24 15:32:53 -0400

    created Utils class

---
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/utils/Utils.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse321.gallery.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Utils {
+
+	private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
+	
+	private <T> Set<T> toSet(Iterable<T> iterable){
+		Set<T> resultList = new HashSet<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
+	
+}
