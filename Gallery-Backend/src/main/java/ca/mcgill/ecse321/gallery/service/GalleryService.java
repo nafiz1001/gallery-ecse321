248c94712a417d2a75b8bae72a23ab6829095884
Thomas <thomasrocks128@me.com>
2020-10-25 20:53:34 -0400

    added isEmailValid method to Utils

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/GalleryService.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/GalleryService.java
@@ -17,69 +17,52 @@ import ca.mcgill.ecse321.gallery.model.Identity;
 import ca.mcgill.ecse321.gallery.model.Listing;
 import ca.mcgill.ecse321.gallery.model.Payment;
 import ca.mcgill.ecse321.gallery.model.PaymentType;
+import ca.mcgill.ecse321.gallery.utils.Utils;
 
 @Service
 public class GalleryService {
 
 	@Autowired
 	GalleryRepository galleryRepository;
-	
+
 	@Autowired
 	AddressRepository addressRepository;
-	
+
 	@Transactional
-	public Optional<Gallery> createGallery(String name, String phoneNumber, Time openingTime, Time closingTime, String email, int commissionPercentage, Address address) {
+	public Optional<Gallery> createGallery(String name, String phoneNumber, Time openingTime, Time closingTime,
+			String email, int commissionPercentage, Address address) {
 		boolean isGalleryValid = true;
 		Gallery gallery = null;
-		
-		//does phoneNumber have exactly 10 digits?
+
+		// does phoneNumber have exactly 10 digits?
 		if (phoneNumber.length() != 10) {
 			isGalleryValid = false;
 		}
-		
-		//does phoneNumber only contain numbers?
+
+		// does phoneNumber only contain numbers?
 		for (int i = 0; i < phoneNumber.length(); i++) {
 			if (Character.isDigit(phoneNumber.charAt(i)) == false) {
 				isGalleryValid = false;
 			}
 		}
-		
-		//is closing time before opening time?
+
+		// is closing time before opening time?
 		if (openingTime.after(closingTime)) {
 			isGalleryValid = false;
 		}
-		
-		//is email valid?
-		int numAts = 0;
-		int atIndex = 0;
-		int numDots = 0;
-		int dotIndex = 0;
-		for (int i = 0; i < email.length(); i++) {
-			if (email.charAt(i) == '@') {
-				numAts++;
-				atIndex = i;
-			}
-			if (email.charAt(i) == '.') {
-				numDots++;
-				dotIndex = i;
-			}
-		}
-		if (numAts != 1) {
-			isGalleryValid = false;
-		}
-		if (numDots != 1 || atIndex >= dotIndex) {
-			isGalleryValid = false;
-		}
-		
-		//is commisionPercentage in between 0 and 100?
+
+		// is email valid?
+		isGalleryValid = Utils.isEmailValid(email);
+
+		// is commisionPercentage in between 0 and 100?
 		if (commissionPercentage <= 0 || commissionPercentage >= 100) {
 			isGalleryValid = false;
 		}
-		
-		//if Gallery is valid
-		if(isGalleryValid) {
-			
-			//create Gallery
+
+		// if Gallery is valid
+		if (isGalleryValid) {
+
+			// create Gallery
 			Gallery validGallery = new Gallery();
 			validGallery.setName(name);
 			validGallery.setPhoneNumber(phoneNumber);
@@ -88,16 +71,14 @@ public class GalleryService {
 			validGallery.setClosingTime(closingTime);
 			validGallery.setCommissionPercentage(commissionPercentage);
 			validGallery.setAddress(address);
-			
+
 			galleryRepository.save(validGallery);
 		}
-		return Optional.ofNullable(gallery);	
+		return Optional.ofNullable(gallery);
 	}
-	
+
 	@Transactional
 	public Optional<Gallery> getGallery(String name) {
 		return galleryRepository.findById(name);
 	}
 }
-	
-	
--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/utils/Utils.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/utils/Utils.java
@@ -23,5 +23,29 @@ public class Utils {
 		}
 		return resultList;
 	}
-
+	
+	public static Boolean isEmailValid(String email) {
+		Boolean isValid = true;
+		int numAts = 0;
+		int atIndex = 0;
+		int numDots = 0;
+		int dotIndex = 0;
+		for (int i = 0; i < email.length(); i++) {
+			if (email.charAt(i) == '@') {
+				numAts++;
+				atIndex = i;
+			}
+			if (email.charAt(i) == '.') {
+				numDots++;
+				dotIndex = i;
+			}
+		}
+		if (numAts != 1) {
+			isValid = false;
+		}
+		if (numDots != 1 || atIndex >= dotIndex) {
+			isValid = false;
+		}
+		return isValid;
+	}
 }
