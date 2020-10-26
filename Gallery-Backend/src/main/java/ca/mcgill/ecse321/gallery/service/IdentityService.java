fe6e45e21fe1a7ee40b8bed68aae1227a85f30b5
Antonia Nistor <antonianistor@hotmail.fr>
2020-10-25 22:13:12 -0400

    createIdentity

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/IdentityService.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/service/IdentityService.java
@@ -2,16 +2,38 @@ package ca.mcgill.ecse321.gallery.service;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ca.mcgill.ecse321.gallery.dao.IdentityRepository;
 import ca.mcgill.ecse321.gallery.model.Identity;
+import ca.mcgill.ecse321.gallery.utils.Utils;
 
 public class IdentityService {
 	@Autowired
 	IdentityRepository identityRepository;
 	
+	public Optional<Identity> createIdentity(String email){
+		boolean emailValid = Utils.isEmailValid(email);
+		if (emailValid == false) throw new IllegalArgumentException("Email entered is not valid");
+		
+		else {
+			Identity identity = new Identity();
+			identity.setEmail(email);
+			
+			identityRepository.save(identity);
+			
+			return Optional.ofNullable(identity);
+		}
+	
+		
+	}
+	
 	public Optional<Identity> getIdentity(String email) {
-		return identityRepository.findById(email);
+		
+		boolean emailValid = Utils.isEmailValid(email);
+	
+		if (emailValid == false) throw new IllegalArgumentException("Email entered is not valid");
+		else return Optional.ofNullable(identityRepository.findIdentityByEmail(email));
 	}
 }
--- Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/service/ListingServiceTests.java
+++ Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/service/ListingServiceTests.java
@@ -1,5 +1,31 @@
 package ca.mcgill.ecse321.gallery.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import ca.mcgill.ecse321.gallery.dao.ArtRepository;
+import ca.mcgill.ecse321.gallery.dao.ListingRepository;
+import ca.mcgill.ecse321.gallery.dao.PaymentRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class ListingServiceTests {
+	
+	@Mock
+	private ListingRepository listingReposiory;
+	
+	@Mock
+	private ArtRepository artRepository;
+	
+	@InjectMocks
+	ListingService listingService;
+	
+	@BeforeEach
+	public void setupMockup() {
+		
+	}
+	
 
 }
