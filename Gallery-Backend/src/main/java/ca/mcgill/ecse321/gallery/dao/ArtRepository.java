a8fc6120f70a076dca0b734d8885dfe87bdd3b18
Ericpelletier135 <ericpelletier135@gmail.com>
2020-11-01 13:46:34 -0500

    Art Id changed to long

--- Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/ArtRepository.java
+++ Gallery-Backend/src/main/java/ca/mcgill/ecse321/gallery/dao/ArtRepository.java
@@ -4,12 +4,15 @@
  */
 package ca.mcgill.ecse321.gallery.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.gallery.model.Art;
 
-public interface ArtRepository extends CrudRepository<Art, String> {
-Art findArtById(String id);
+public interface ArtRepository extends CrudRepository<Art, Long> {
+	Art findArtById(Long id);
 	
 	void deleteAll();
+
 }
