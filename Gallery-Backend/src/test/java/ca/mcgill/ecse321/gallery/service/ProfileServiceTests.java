fc18403f697378149e55306a30b22143acee7453
Ericpelletier135 <ericpelletier135@gmail.com>
2020-11-01 15:04:02 -0500

    ProfileServiceTest

--- Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/service/ProfileServiceTests.java
+++ Gallery-Backend/src/test/java/ca/mcgill/ecse321/gallery/service/ProfileServiceTests.java
@@ -43,33 +43,25 @@ public class ProfileServiceTests {
 	@InjectMocks
 	ProfileService profileService;
 	
-	private String id = "0";
 	private HashSet<Account> savedAccounts = new HashSet<>();
 	private HashSet<Profile> savedProfiles = new HashSet<>();
 	
 	@BeforeEach
 	public void setupMockup() {
 		
-		id = "0";
-		
 		// database mock
 		savedAccounts.clear();
 		savedProfiles.clear();
 		
-//lambda for simulating saving profile
+		//lambda for simulating saving profile
 
 		Answer<Profile> updateProfileNumberAndReturn = (InvocationOnMock invocation) -> {
 			Profile a = (Profile)invocation.getArgument(0);
-			a.setId(id);
 			savedProfiles.add(a);
-			int profileNumberAsNum = Integer.parseInt(id);
-			profileNumberAsNum++;
-			id = Integer.toString(profileNumberAsNum);
 			return a;
+		};
 
-};
-
-//lambda for simulating saving account
+		//lambda for simulating saving account
 		Answer<Account> saveAndReturn = (InvocationOnMock invocation) -> {
 			Account i = (Account)invocation.getArgument(0);
 			savedAccounts.add(i);
@@ -77,26 +69,26 @@ public class ProfileServiceTests {
 		};
 		
 		// simulate saving profile
-				lenient().when(profileRepository.save(any(Profile.class))).then(updateProfileNumberAndReturn);
+		lenient().when(profileRepository.save(any(Profile.class))).then(updateProfileNumberAndReturn);
+		
+		// simulate saving account
+		lenient().when(accountRepository.save(any(Account.class))).thenAnswer(saveAndReturn);
+		
+		
+		// simulate finding profile by Id
+		lenient().when(profileRepository.findById(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+			String Id = ((String)invocation.getArgument(0));
+			for (Profile a : savedProfiles) {
+				if (a.getId().equals(Id))
+					return Optional.ofNullable(a);
+			}
+			return Optional.empty();
+		});
 				
-				// simulate saving account
-				lenient().when(accountRepository.save(any(Account.class))).thenAnswer(saveAndReturn);
-				
-				
-				// simulate finding profile by Id
-				lenient().when(profileRepository.findById(anyString())).thenAnswer((InvocationOnMock invocation) -> {
-					String Id = ((String)invocation.getArgument(0));
-					for (Profile a : savedProfiles) {
-						if (a.getId().equals(Id))
-							return Optional.ofNullable(a);
-					}
-					return Optional.empty();
-				});
-						
-				// simulate finding all Profiles
-				lenient().when(profileRepository.findAll()).thenAnswer((InvocationOnMock invocation) -> {
-					return savedProfiles;
-				});
+		// simulate finding all Profiles
+		lenient().when(profileRepository.findAll()).thenAnswer((InvocationOnMock invocation) -> {
+			return savedProfiles;
+		});
 				
 		
 		
@@ -116,31 +108,112 @@ public class ProfileServiceTests {
 		
 		assertTrue(profile.isPresent());
 
-
 	}
 	
 	@Test
 	void testBioViolation() {
+		boolean exceptionCaught1 = false;
+		boolean exceptionCaught2 = false;
+		ArrayList<Listing> listings = new ArrayList<Listing>();
+		Account account = new Account();
+		account.setUsername("Eric");
+		ArrayList<Art> arts = new ArrayList<Art>();
 		
+		try {
+			Optional<Profile> profile = profileService.createProfile("  ", "pic", listings, account, "EricPelletier", arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught1 = true;
+		}
+		
+		assertTrue(exceptionCaught1);
+		assertEquals(0,profileService.getAllProfiles().size());
+		
+		try {
+			Optional<Profile> profile = profileService.createProfile(null, "pic", listings, account, "EricPelletier", arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught2 = true;
+		}
+		assertTrue(exceptionCaught2);
+		assertEquals(0,profileService.getAllProfiles().size());
+		
+	}
+	
+	@Test
+	void testFullnameViolation() {
+		boolean exceptionCaught1 = false;
+		boolean exceptionCaught2 = false;
+		ArrayList<Listing> listings = new ArrayList<Listing>();
+		Account account = new Account();
+		account.setUsername("Eric");
+		ArrayList<Art> arts = new ArrayList<Art>();
+		
+		try {
+			Optional<Profile> profile = profileService.createProfile("This is the bio.", "pic", listings, account, " ", arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught1 = true;
+		}
+		
+		assertTrue(exceptionCaught1);
+		assertEquals(0,profileService.getAllProfiles().size());
+		
+		try {
+			Optional<Profile> profile = profileService.createProfile("This is the bio.", "pic", listings, account, null, arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught2 = true;
+		}
+		assertTrue(exceptionCaught2);
+		assertEquals(0,profileService.getAllProfiles().size());
+	}
+	
+	@Test
+	void testPictureViolation() {
+		boolean exceptionCaught1 = false;
+		boolean exceptionCaught2 = false;
+		ArrayList<Listing> listings = new ArrayList<Listing>();
+		Account account = new Account();
+		account.setUsername("Eric");
+		ArrayList<Art> arts = new ArrayList<Art>();
+		
+		try {
+			Optional<Profile> profile = profileService.createProfile("This is the bio.", "  ", listings, account, "Eric Pelletier", arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught1 = true;
+		}
+		
+		assertTrue(exceptionCaught1);
+		assertEquals(0,profileService.getAllProfiles().size());
+		
+		try {
+			Optional<Profile> profile = profileService.createProfile("This is the bio.", null, listings, account, "Eric Pelletier", arts);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionCaught2 = true;
+		}
+		assertTrue(exceptionCaught2);
+		assertEquals(0,profileService.getAllProfiles().size());
 	}
 	
 	
 	@Test
 	void testGetProfile() {
-		assertTrue(profileService.getProfile("0").isEmpty());
+		assertTrue(profileService.getProfile("Haluk:Haluk Calin").isEmpty());
 		
 		String bio = "Hello, welcome to my bio";
 		String picture = "pic";
 		ArrayList<Listing> listings = new ArrayList<Listing>();
 		Account account = new Account();
+		account.setUsername("Haluk");
 		String fullname = "Haluk Calin";
 		ArrayList<Art> arts = new ArrayList<Art>();
 		
 		Optional<Profile> profile = profileService.createProfile(bio, picture, listings, account, fullname, arts);
 		
-		assertTrue(profileService.getProfile("0").isPresent());
-
-		
+		assertTrue(profileService.getProfile(profile.get().getAccount().getUsername() + ":" + profile.get().getFullname()).isPresent());
 	}
 	
 	@Test
@@ -151,13 +224,13 @@ public class ProfileServiceTests {
 		String picture = "pic";
 		ArrayList<Listing> listings = new ArrayList<Listing>();
 		Account account = new Account();
+		account.setUsername("Eric");
 		String fullname = "Haluk Calin";
 		ArrayList<Art> arts = new ArrayList<Art>();
 		
 		Optional<Profile> profile1 = profileService.createProfile(bio, picture, listings, account, fullname, arts);
 		
-		bio = "Hello";
-		
+		fullname = "EricPelletier";
 		
 		Optional<Profile> profile2 = profileService.createProfile(bio, picture, listings, account, fullname, arts);
 
