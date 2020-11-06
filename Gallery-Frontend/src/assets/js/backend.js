9d79003fd736a773171cc3c4c1f2064e87971d31
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-06 10:41:33 -0500

    initial test with Checkout.vue

---
+++ Gallery-Frontend/src/assets/js/backend.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const config = require('../../../config')
+
+const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+const AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+  AXIOS: AXIOS
+}
---
+++ Gallery-Frontend/src/assets/js/dtos.js
@@ -0,0 +1,11 @@
+function AddressDto(streetNumber, street, city, province, postalCode) {
+    this.streetNumber = streetNumber;
+    this.street = street;
+    this.city = city;
+    this.province = province;
+    this.postalCode = postalCode;
+}
+
+export default {
+    AddressDto: AddressDto
+}
