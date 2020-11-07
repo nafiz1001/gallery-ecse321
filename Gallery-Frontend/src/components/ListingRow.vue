06d3bc5ccff32a64333f557155e188d3b49c2c52
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-07 16:30:14 -0500

    include pickup and shipping in listing row

--- Gallery-Frontend/src/components/ListingRow.vue
+++ Gallery-Frontend/src/components/ListingRow.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="listing-row">
-        <img class="listing-row-img" :src="image">
+        <img class="listing-row-img" :src="getListing(id).image">
         <div class="listing-row-info">
-            <div>
-                <span style="font-weight: bold">{{title}}</span> By {{author}}</div>
-            <div>{{price}}$</div>
+            <div><span style="font-weight: bold">{{getListing(id).title}}</span> By {{getListing(id).author}}</div>
+            <div>Pick Up {{getListing(id).canPickUp ? "&#x2705;" : "&#x274C;"}}</div>
+            <div>Shipping {{getListing(id).canDeliver ? "&#x2705;" : "&#x274C;"}}</div>
+            <div>{{getListing(id).price}}$</div>
         </div>
     </div>
 </template>
@@ -18,7 +19,7 @@
     padding: 0.5em 0.5em 0.5em 0.5em;
 }
 .listing-row > img {
-    height: 4em;
+    height: 6em;
     margin-right: 1em;
 }
 
@@ -37,21 +38,16 @@ import DTOs from '../assets/js/dtos'
 export default {
   name: 'listingrow',
   props: ['id'],
-  data: () => {
-      return {
-          title: 'Mr. Fishy',
-          image: 'https://pbs.twimg.com/profile_images/2996390845/d5f215b28cfce7c235080c37f54b05fb_400x400.jpeg',
-          price: 69,
-          author: 'Barack Obama'
-      }
-  },
   methods: {
       getListing: (id) => {
           return {
-              id: 0,
-              title: 'Mr. Fishy',
-              image: 'https://pbs.twimg.com/profile_images/2996390845/d5f215b28cfce7c235080c37f54b05fb_400x400.jpeg',
-              price: 69
+            title: 'Mr. Fishy',
+            image: 'https://pbs.twimg.com/profile_images/2996390845/d5f215b28cfce7c235080c37f54b05fb_400x400.jpeg',
+            price: 69,
+            author: 'Barack Obama',
+            canPickUp: id === 0,
+            canDeliver: id === 1,
+            id: id
           }
       }
   }
