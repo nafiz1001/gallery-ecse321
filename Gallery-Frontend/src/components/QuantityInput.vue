793f70a8b9a9310833220790d1e24e963e8c5da7
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-08 19:20:00 -0500

    add QuantityInput.vue

---
+++ Gallery-Frontend/src/components/QuantityInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="number" min="1" :max=maxquantity step="1" :value="value" required>
+</template>
+
+<script>
+function getQuantity(id) {
+        return id + 1;
+}
+
+export default {
+    props: ['id', 'value'],
+    data() {
+        return {
+            maxquantity: getQuantity(this.id)
+        }
+    }
+}
+</script>
