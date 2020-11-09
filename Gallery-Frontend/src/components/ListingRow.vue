9a477f73dbed7073837bf4e0414af10ec449e450
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-08 19:20:49 -0500

    make ListingRow more efficient

--- Gallery-Frontend/src/components/ListingRow.vue
+++ Gallery-Frontend/src/components/ListingRow.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="listing-row">
-        <img class="listing-row-img" :src="getListing(id).image">
+        <img class="listing-row-img" :src="listing.image">
         <div class="listing-row-info">
-            <div><span style="font-weight: bold">{{getListing(id).title}}</span> By {{getListing(id).author}}</div>
-            <div>Pick Up {{getListing(id).canPickUp ? "&#x2705;" : "&#x274C;"}}</div>
-            <div>Shipping {{getListing(id).canDeliver ? "&#x2705;" : "&#x274C;"}}</div>
-            <div>{{getListing(id).price}}$</div>
+            <div><span style="font-weight: bold">{{listing.title}}</span> By {{listing.author}}</div>
+            <div>Pick Up {{listing.canPickUp ? "&#x2705;" : "&#x274C;"}}</div>
+            <div>Shipping {{listing.canDeliver ? "&#x2705;" : "&#x274C;"}}</div>
+            <div>{{listing.price}}$</div>
+            <div>Max Quantity: {{listing.quantity}}</div>
         </div>
     </div>
 </template>
@@ -13,13 +14,12 @@
 <style>
 .listing-row {
     display: flex;
-    justify-content: space-around;
-    border: 1px solid black;
+    justify-content: space-between;
     margin-bottom: 1em;
     padding: 0.5em 0.5em 0.5em 0.5em;
 }
 .listing-row > img {
-    height: 6em;
+    height: 8em;
     margin-right: 1em;
 }
 
@@ -35,20 +35,25 @@
 import Backend from '../assets/js/backend'
 import DTOs from '../assets/js/dtos'
 
+function getListing(id) {
+    return {
+    title: 'Mr. Fishy',
+    image: 'https://pbs.twimg.com/profile_images/2996390845/d5f215b28cfce7c235080c37f54b05fb_400x400.jpeg',
+    price: 69,
+    author: 'Barack Obama',
+    canPickUp: id === 0,
+    canDeliver: id === 1,
+    id: id,
+    quantity: id + 1
+    }
+}
+
 export default {
   name: 'listingrow',
   props: ['id'],
-  methods: {
-      getListing: (id) => {
-          return {
-            title: 'Mr. Fishy',
-            image: 'https://pbs.twimg.com/profile_images/2996390845/d5f215b28cfce7c235080c37f54b05fb_400x400.jpeg',
-            price: 69,
-            author: 'Barack Obama',
-            canPickUp: id === 0,
-            canDeliver: id === 1,
-            id: id
-          }
+  data: () => {
+      return {
+          listing: getListing(this.id)
       }
   }
 }
