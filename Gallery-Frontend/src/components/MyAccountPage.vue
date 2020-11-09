abe68c2676e95af690daaaa3b19396790db565f3
HALUK CALIN <haluk.calin@mail.mcgill.ca>
2020-11-09 17:33:47 +0300

    MyAccountPage

---
+++ Gallery-Frontend/src/components/MyAccountPage.vue
@@ -0,0 +1,84 @@
+<template>
+<form action="action_page.php">
+	<div id="Account">
+	<div class="container">
+		<h1>My Account</h1>
+		<hr>
+
+	<div class="username">
+        <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+          Username:
+        </h3>
+        <p style="display: inline; font-size: 22px; font-family: Raleway">
+          BestUsername
+        </p>
+		<a class="edit"> Edit Usersame</a>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Password:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+        *********
+      </p>
+      <a class="edit"> Edit Password</a>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Date Joined:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+        01/09/2020
+      </p>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Date of Birth:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+		  19/12/1998
+		  </p>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Account Number:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+        123456789
+      </p>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Revenu ($):
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+        $ -1000
+      </p>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Address:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">
+        845 Sherbrooke St W, Montreal, Quebec H3A 0G4, Canada
+      </p>
+	  <a class="edit"> Edit Address</a>
+      <div class="passline"></div>
+      <h3 style="display: inline; font-family: Raleway; font-weight: bold">
+        Payment Type:
+      </h3>
+      <p style="display: inline; font-size: 22px; font-family: Raleway">Credit</p>
+      <div class="passline"></div>
+    </div>
+</template>
+
+<style>
+
+.edit {
+  display: inline;
+  font-family: Raleway;
+  font-size: 20px;
+
+  color: #000000;
+  margin: 8px;
+  border: 2px solid #000000;
+  padding: 3px;
+}
+.edit:hover {
+  color: #000000;
+}
+</style>
