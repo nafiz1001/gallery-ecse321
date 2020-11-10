32e244edc338efb153dc8b555fffd6e136cb8489
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-10 09:08:22 -0500

    confirmation componentt

---
+++ Gallery-Frontend/src/components/Confirmation.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="confirmation">
+        <h2>Your transaction has been accepted</h2>
+        <div>Your confirmation number: {{confirmationnumber}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['confirmationnumber']
+}
+</script>
