62214f0bc90fd594666a4cc058af473a83454502
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-13 20:33:09 -0500

    create art.js

--- Gallery-Frontend/src/assets/js/art.js
+++ Gallery-Frontend/src/assets/js/art.js
@@ -2,10 +2,14 @@ import Backend from './backend'
 
 
 async function createArt(artDto){
-    const res = await Backend.createArt(artDto);
+    const res = await Backend.createArt(artDto, "A123456");
     if (res) {
         return res.data;
     }
     return null;
+}
+
+export default {
+    createArt: createArt
 }
 
