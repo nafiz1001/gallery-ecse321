bcfeb8fab43c7aadf79ee79d471eff1956a5b24a
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-13 20:33:43 -0500

    create listing.js

--- Gallery-Frontend/src/assets/js/listing.js
+++ Gallery-Frontend/src/assets/js/listing.js
@@ -1,9 +1,13 @@
 import Backend from './backend'
 
 async function createListing(listingDto){
-    const res = await Backend.createListing(listingDto);
+    const res = await Backend.createListing(listingDto, "A123456");
     if (res){
         return res.data;
     }
     return null;
+}
+
+export default {
+    createListing: createListing
 }
