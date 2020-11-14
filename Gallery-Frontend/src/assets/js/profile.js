90ed86a3c72d58e5f2be0188e7b5e2dfebe343a8
Thomas <thomasrocks128@me.com>
2020-11-14 12:10:59 -0500

    Started work on ArtistSignIn

--- Gallery-Frontend/src/assets/js/profile.js
+++ Gallery-Frontend/src/assets/js/profile.js
@@ -6,6 +6,27 @@ async function createProfile(profileDto, password) {
     return await Backend.createProfile(profileDto, password);
 }
 
+function getProfile() {
+    const profileString = localStorage.getItem('profile');
+    if (profileString) {
+        return JSON.parse(profileString);
+    } else {
+        return null;
+    }
+}
+
+async function loadProfileFromDatabase(id) {
+    const response = await Backend.getProfile(id).catch(error => console.error(error));
+    if (response) {
+        console.log(response.data);
+        localStorage.setItem('profile', JSON.stringify(response.data));
+    }
+
+    return response.data;
+}
+
 export default {
-    createProfile: createProfile
+    createProfile: createProfile,
+    loadProfileFromDatabase: loadProfileFromDatabase,
+    getProfile: getProfile
 }
