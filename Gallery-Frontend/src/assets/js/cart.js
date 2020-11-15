1156af427ab075264b3c6241f9e76df8a11a5e96
nafiz1001 <nafiz.islam1001@gmail.com>
2020-11-15 18:21:31 -0500

    cart wip

--- Gallery-Frontend/src/assets/js/cart.js
+++ Gallery-Frontend/src/assets/js/cart.js
@@ -1,3 +1,4 @@
+import { resolve } from '../../../config/dev.env';
 import Backend from './backend'
 
 function getCart() {
@@ -12,18 +13,19 @@ function getCart() {
   
   function addToCart(id, quantity) {
     const currQuantity = getQuantity(id);
-    const maxQuantity = Backend.getListing(id).data.quantity;
-
-    if (currQuantity + quantity > maxQuantity) {
-        return false;
-    } else {
-        setQuantity(id, getQuantity(id) + Number(quantity))
-        return true;
-    }
+    return Backend.getListing(id).then(response => {
+      const maxQuantity = response.data.quantity;
+      if (currQuantity + quantity > maxQuantity) {
+        throw new Error(`You've exceeded the max quantity of the listing with id ${id}`);
+      } else {
+          setQuantity(id, getQuantity(id) + Number(quantity))
+          return id;
+      }
+    });
   }
   
 function getQuantity(id) {
-    let cart = getCart(id);
+    let cart = getCart();
     let quantity = 0;
 
     if (id in cart)
@@ -33,7 +35,7 @@ function getQuantity(id) {
 }
 
 function setQuantity(id, quantity) {
-    let cart = getCart(id);
+    let cart = getCart();
   
     //console.log(quantity);
     cart[id] = {
